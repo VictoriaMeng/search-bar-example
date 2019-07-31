b279c6442abaaf665cc4c72c86c1f49dd6c7d2e4
VictoriaMeng <vym205@protonmail.com>
2019-07-31 10:26:50 -0500

    Update instructions

--- places.js
+++ places.js
@@ -1,4 +1,4 @@
-// Your event handler function should be defined here with an event parameter
+// Your event handler should be defined here with an event parameter
 // Call event.preventDefault() at the start of your function to prevent the default url change that may block the other code in your function.
 // Use <id>.value to select the input id of your search bar that you defined in the HTML page.
 // For example, if you gave the <input> tag the 'query' id, call query.value
