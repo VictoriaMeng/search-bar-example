3a8b4466eea3247e8f258de4afac206de79c69c7
VictoriaMeng <vym205@protonmail.com>
2019-08-01 09:52:16 -0500

    Updated comments

--- script.js
+++ script.js
@@ -3,6 +3,17 @@
 // Use <id>.value to select the input id of your search bar that you defined in the HTML page.
 // For example, if you gave the <input> tag the 'query' id, call query.value
 
+// The fetch function takes a string url as an argument. This is the url the GET request will be sent to.
+// The url is formatted w/ string interpolation - ie., adding variables to your string.
+// In Javascript, you use string interpolation be using backticks to make strings instead of double-quotes.
+// Then, you place the variable in between ${}.
+
+// The two then() functions are evoked one by one once the GET request receives a response from the Youtube servers.
+// The then() function takes a function as an argument. This function is evoked when then() is called.
+// Below, you'll see two functions defined without names. They're called 'anonymous functions'.
+// The response from the fetch request is passed into the anonymous functions as arguments.
+//
+
 function getResults(event) {
   event.preventDefault();
   fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&q=${query.value}&type=channel&key=AIzaSyDA7HqRQRDt7KAblpWYrm12u5sEpNoqBVE`)
